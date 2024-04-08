fa9e2d9c608c258fd75b440ff5d2e1c309623629
masato matsuoka <masato.matsuoka@example.com>
2024-04-08 07:28:13 +0000

    break_continue.php

---
+++ breake_continue.php
@@ -0,0 +1,21 @@
+<?php
+
+for($i=0; $i<10; ++$i){
+    echo "{$i}は奇数かな？\n";
+    if(($i%2)===1){
+        echo "奇数です\n";
+
+        continue;
+    }
+    echo "偶数です\n";
+}
+
+$a=0;
+while($a<10){
+    echo "{$a} \n";
+    if($a>=5){
+        echo "{$a}　以上なのでループを抜けます。\n";
+        break;
+    }
+    ++$a;
+}

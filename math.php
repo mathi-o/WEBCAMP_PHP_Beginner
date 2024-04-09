c29bcf3fb2ea9fccf3ca0dd0942ef60962f92fd0
masato matsuoka <masato.matsuoka@example.com>
2024-04-09 04:45:21 +0000

    math.php

---
+++ math.php
@@ -0,0 +1,12 @@
+<?php
+$i=-100;
+$j=abs($i);
+echo "{$i}の絶対値は{$j}\n";
+
+$f = 3.5;
+// 端数の切り捨て
+$f_floor = floor($f);
+echo "{$f} の端数を切り捨てると {$f_floor} \n";
+// 端数の切り上げ
+$f_ceil = ceil($f);
+echo "{$f} の端数を切り上げると {$f_ceil} \n";

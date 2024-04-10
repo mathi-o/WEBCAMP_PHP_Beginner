9b0f744020d2c4d21ade31a532dc1cd73af9b6f8
masato matsuoka <masato.matsuoka@example.com>
2024-04-10 05:06:52 +0000

    class_2.php

---
+++ class_2.php
@@ -0,0 +1,20 @@
+<?php
+
+class Myclass{
+
+    //オブジェクト定数の宣言
+    const HOGE = 'string';
+
+    public function __construct(){
+
+        echo "コンストラクトが動いた\n";
+    }
+
+    public function __destruct(){
+        echo "デストラクトが動いた\n";
+    }
+}
+
+var_dump(Myclass::HOGE); // 「::」：スコープ定義演算子
+
+$obj = new Myclass();
